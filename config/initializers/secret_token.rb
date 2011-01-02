91ae6b2d04863e58b851a95b35194fdd00e7db93
JF Plante <jfrederic.plante@gmail.com>
2011-01-01 21:15:52 -0800

    le vocab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panavoc::Application.config.secret_token = 'c02988a1e493b820d62098b3f617d8b7f0fd9ae4fb7e9f9479b798dc5fc40e19c1e0e84e8749d86ee7053d4aa780c78d7ba1b7168ceded79c6985008dd8c36ba'

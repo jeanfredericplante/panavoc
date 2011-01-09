649c75c5f590f9cc5bf976e5f25ff83be26c60bf
Jean Frederic Plante <jfrederic.plante@gmail.com>
2011-01-09 13:09:39 -0800

    added partial for word entries with div id corresponding to the eng word as a key

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,19 @@ class HomeController < ApplicationController
   def index
     @vocentries = Vocentry.select_home_words(:maximum => 5)
 
+   # jQuery.getJSON("http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=test&sl=en&tl=en&restrict=pr%2Cde&client=te",'',get_definition)  
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @vocentries }
     end
  
   end
+  def get_definition
+    alert("hello")
+    # @vocentries.each do |v|
+    # end
+  end
+
 
 end
